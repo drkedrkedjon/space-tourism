076769728f877bf5666416c39c60c9039ba21a06
Sasa Savic Perusina <sasa@bubulazy.com>
2022-05-07 18:41:38 +0100

    Nevegacion casi terminada, falta accesibilidad

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const nav = document.querySelector('.primary-navigation')
-const navTogle = document.querySelector('.mobile-nav-toggle')
+const navToggle = document.querySelector('.mobile-nav-toggle')
 
-navTogle.addEventListener('click', () => {
+navToggle.addEventListener('click', () => {
   const visibility = nav.getAttribute('data-visible')
   if(visibility === 'false') {
-    nav.setAttribute('data-visible', 'true')
+    nav.setAttribute('data-visible', true)
+    navToggle.setAttribute("aria-expanded", true);
   } else {
-    nav.setAttribute('data-visible', 'false')
+    nav.setAttribute('data-visible', false)
+    navToggle.setAttribute("aria-expanded", false);
   }
 })
